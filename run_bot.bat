2ba4f42eea5e5b99444e79b94482f40850794a85
gnrodrigues <35896774+gnrodrigues@users.noreply.github.com>
2026-03-06 07:50:41 -0300

    Create run_bot.bat

---
+++ run_bot.bat
@@ -0,0 +1,21 @@
+@echo off
+title WhatsApp Poll Bot
+
+echo =====================================
+echo        WhatsApp Poll Bot
+echo =====================================
+echo.
+
+echo Acessando pasta do projeto...
+cd /d C:\caminho\para\whatsapp-enquete-bot
+
+echo.
+echo Executando bot...
+python bot.py
+
+echo.
+echo =====================================
+echo Execucao finalizada
+echo =====================================
+
+pause
